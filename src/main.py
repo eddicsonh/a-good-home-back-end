47850536c95928ad5a4c459acdf1a9af202396ed
Ehernandez_Matrix <ehernandez@matrixcpmsolutions.com>
2021-06-14 22:19:38 -0400

    Se crea EndPoint para la creacion de Inmuebles

--- src/main.py
+++ src/main.py
@@ -2,13 +2,14 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, json, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from werkzeug.wrappers import response
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Real_state
 #from models import Person
 
 app = Flask(__name__)
@@ -35,6 +36,44 @@ def handle_hello():
 
     response_body = {
         "msg": "Hello, this is your GET /user response "
+    }
+
+    return jsonify(response_body), 200
+
+@app.route('/real_state/all', methods=['GET'])
+def handle_real_state_get_all():
+    get_real_state = Real_state.query.all()
+    all_real_state_serialize = []
+    for real_state in get_real_state:
+        all_real_state_serialize.append(real_state.serialize())
+    response_body = {
+        "status": "OK",
+        "response": all_real_state_serialize
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    return jsonify(response_body), 200
+
+@app.route('/real_state', methods=['POST'])
+def handled_real_state_create():
+
+    body = request.json
+    new_real_state = Real_state(
+        body["name"],
+        body["description"],
+        body["location"],
+        body["total_area"],
+        body["builded_surface"],
+        body["rooms"],
+        body["bathrooms"],
+        body["parkings"]
+    )
+    db.session.add(new_real_state)
+    db.session.commit()
+
+    response_body = {
+        "status": "ok"
     }
 
     return jsonify(response_body), 200
--- src/models.py
+++ src/models.py
@@ -1,3 +1,5 @@
+from enum import unique
+from operator import truediv
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -17,3 +19,38 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Real_state(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=False, nullable=False)
+    description = db.Column(db.String(200), unique=False, nullable=True)
+    location = db.Column(db.String(500), unique=False, nullable=True)
+    total_area = db.Column(db.Integer, unique=False, nullable=True)
+    builded_surface = db.Column(db.Integer, unique=False, nullable=True)
+    rooms = db.Column(db.Integer, unique=False, nullable=True)
+    bathrooms = db.Column(db.Integer, unique=False, nullable=True)
+    parkings = db.Column(db.Integer, unique=False, nullable=True)
+
+    def __init__ (self, name, description, location, total_area, builded_surface, rooms, bathrooms, parkings):
+        self.name = name,
+        self.description = description,
+        self.location = location,
+        self.total_area = total_area,
+        self.builded_surface = builded_surface,
+        self.rooms = rooms,
+        self.bathrooms = bathrooms,
+        self.parkings = parkings
+
+    def serialize(self):
+        return{
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "location": self.location,
+            "total_area": self.total_area,
+            "builded_surface": self.builded_surface,
+            "rooms": self.rooms,
+            "bathrooms": self.bathrooms,
+            "parkings": self.parkings
+        }
+
