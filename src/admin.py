021b8b9b34ebe729693c1287f4735f1ffaa417b3
Eddicsonh <eddicsonh@gmail.com>
2021-06-22 14:56:21 -0400

    Se crea endpoint de busqueda por locacion de inmueble

--- src/admin.py
+++ src/admin.py
@@ -1,6 +1,6 @@
 import os
 from flask_admin import Admin
-from models import db, User, RealState, Agent
+from models import db, User, RealState, Agent, Transaction
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,3 +13,4 @@ def setup_admin(app):
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(RealState, db.session))
     admin.add_view(ModelView(Agent, db.session))
+    admin.add_view(ModelView(Transaction, db.session))
